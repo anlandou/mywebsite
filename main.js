f8d0ca9d57a730408eaebdd4f5d05c614ab807bb
anlandou <avemaria.nlandou@gmail.com>
2020-03-21 03:15:16 -0400

    My new Website

---
+++ main.js
@@ -0,0 +1,28 @@
+let popup1 = document.getElementById("popup1");
+let popup2 = document.getElementById("popup2");
+let popup3 = document.getElementById("popup3");
+
+function aboutme() {
+    popup1.style.display = "block";
+}
+
+
+function projects() {
+    popup2.style.display = "block"
+}
+
+function dailylogs() {
+    popup3.style.display = "block"
+}
+function close1() {
+    popup1.style.display = "none";
+}
+
+function close2() {
+    popup2.style.display = "none";
+}
+
+ function close3() {
+    popup3.style.display = "none";
+}
+
