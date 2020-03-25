4580892c06768d649e67027e0cdacb7721b58df3
anlandou <avemaria.nlandou@gmail.com>
2020-03-25 04:08:41 -0400

    added link todolist app

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 let popup1 = document.getElementById("popup1");
 let popup2 = document.getElementById("popup2");
 let popup3 = document.getElementById("popup3");
+let body = document.getElementsById("body");
 
 function aboutme() {
+    closeAll(); 
     popup1.style.display = "block";
+    
 }
 
 
 function projects() {
-    popup2.style.display = "block"
-    if(dailylogs() == true){
-        popup3.style.display = "none";
-    }
+    closeAll(); 
+    popup2.style.display = "block";
 }
 
 function dailylogs() {
-    popup3.style.display = "block"
+    closeAll(); 
+    popup3.style.display = "block";
 }
 function close1() {
     popup1.style.display = "none";
@@ -29,3 +31,10 @@ function close2() {
     popup3.style.display = "none";
 }
 
+//close click on outside 
+
+function closeAll(){
+    popup1.style.display = "none";
+    popup2.style.display = "none";
+    popup3.style.display = "none";
+}
